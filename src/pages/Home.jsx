7dba646e1d142e27012a036558d39a143cd1d99e
Alejandro Feijoo <alejandro.feijoo@udarealestate.com>
2023-09-06 16:52:29 +0200

    Add fist municipalities fetch for weather cards
    More detailed description of the change:
    - Add the first calls in Home to use the first weather cards with real weather data of the municipalities.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Selector from "../components/Selector";
+import WeatherCard from "../components/WeatherCard";
+import { getWeatherIcon } from "../utils/getWeatherIcon";
 
 const StyledWrapper = styled.div`
-	height: 100vh;
-	width: 100vw;
+	width: 88vw;
+	height: auto;
+	margin: 0 6vw;
+	display: flex;
+	flex-direction: column;
+	gap: 2vh;
 `;
 
-const StyledHeader = styled.div`
-	height: 40%;
+const StyledHeader = styled.section`
+	height: 40vh;
 	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	gap: 5vh;
 `;
 
-const StyledBody = styled.div`
-	height: 60%;
+const StyledBody = styled.section`
+	height: auto;
 	width: 100%;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+	gap: 2vh;
 `;
 
 export default function Home() {
 	const [provinces, setProvinces] = useState([]);
 	const [selectedProvince, setSelectedProvince] = useState("");
+	const [municipalities, setMunicipalities] = useState([]);
 
 	useEffect(() => {
 		fetch("https://www.el-tiempo.net/api/json/v2/provincias")
@@ -37,16 +52,51 @@ export default function Home() {
 			});
 	}, []);
 
+	useEffect(() => {
+		if (!selectedProvince) return;
+		fetch(`https://www.el-tiempo.net/api/json/v2/provincias/${selectedProvince}`)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data.ciudades);
+				setMunicipalities(data.ciudades);
+			})
+			.catch((error) => {
+				console.error("Error al obtener datos:", error);
+			});
+	}, [selectedProvince]);
+
 	const onSelectedProvince = (value) => {
 		setSelectedProvince(value);
 	};
 
 	return (
 		<StyledWrapper>
-			<StyledHeader>
-				<Selector label="Provincia" options={provinces} onSelect={onSelectedProvince} />
-			</StyledHeader>
-			<StyledBody>Body</StyledBody>
+			{provinces.length ? (
+				<>
+					<StyledHeader>
+						<h1>El tiempo en España</h1>
+						<Selector label="Provincia" options={provinces} onSelect={onSelectedProvince} />
+					</StyledHeader>
+					{municipalities.length ? (
+						<StyledBody>
+							{municipalities.map((municipality) => (
+								<WeatherCard
+									key={municipality?.id}
+									name={municipality?.name}
+									weather={municipality?.stateSky?.description}
+									icon={getWeatherIcon(municipality?.stateSky?.description, "large")}
+									max={`${municipality?.temperatures?.max}°C`}
+									min={`${municipality?.temperatures?.min}°C`}
+								/>
+							))}
+						</StyledBody>
+					) : (
+						<h1>No hay resultados</h1>
+					)}
+				</>
+			) : (
+				<h1>Cargando...</h1>
+			)}
 		</StyledWrapper>
 	);
 }
