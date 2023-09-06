954c248e84819a5fa406dfdc3cb306ae00fe20cc
Alejandro Feijoo <alejandro.feijoo@udarealestate.com>
2023-09-06 15:44:33 +0200

    Home page first fetch with selector
    More detailed description of the change:
    - The Home page with the first steps of styling, fetching of the provinces and the use of the Selector component working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,52 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Selector from "../components/Selector";
+
+const StyledWrapper = styled.div`
+	height: 100vh;
+	width: 100vw;
+`;
+
+const StyledHeader = styled.div`
+	height: 40%;
+	width: 100%;
+`;
+
+const StyledBody = styled.div`
+	height: 60%;
+	width: 100%;
+`;
+
 export default function Home() {
-	return <div>Home</div>;
+	const [provinces, setProvinces] = useState([]);
+	const [selectedProvince, setSelectedProvince] = useState("");
+
+	useEffect(() => {
+		fetch("https://www.el-tiempo.net/api/json/v2/provincias")
+			.then((response) => response.json())
+			.then((data) => {
+				setSelectedProvince(data?.provincias[0]?.CODPROV);
+				const allProvinces = data?.provincias?.map((province) => ({
+					value: province?.CODPROV,
+					text: province?.NOMBRE_PROVINCIA,
+				}));
+				setProvinces(allProvinces);
+			})
+			.catch((error) => {
+				console.error("Error al obtener datos:", error);
+			});
+	}, []);
+
+	const onSelectedProvince = (value) => {
+		setSelectedProvince(value);
+	};
+
+	return (
+		<StyledWrapper>
+			<StyledHeader>
+				<Selector label="Provincia" options={provinces} onSelect={onSelectedProvince} />
+			</StyledHeader>
+			<StyledBody>Body</StyledBody>
+		</StyledWrapper>
+	);
 }
