48ebb1d4a8f2d802ed15b808893642052f877d38
Alejandro Feijoo <alejandro.feijoo@udarealestate.com>
2023-09-07 13:35:55 +0200

    New Detail Page & paths
    More detailed description of the change:
    - New page for city weather details and the new route created

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { paths } from "./paths";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
+import Detail from "../pages/Detail";
 
 export default function Router() {
 	return (
 		<Routes>
 			<Route path={paths.home} element={<Home />} index />
+			<Route path={paths.detail} element={<Detail />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
