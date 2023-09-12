5c3a0d08886b8e4f78f5da83d34d665165e8dda0
Alejandro Feijoo <alejandro.feijoo@udarealestate.com>
2023-09-12 13:39:45 +0200

    feature: Add gh pages to repository
    More detailed description of the change:
    - Install and configure gh-pages package to prepare the deploy in GitHub

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/Weekly-weather/",
 	test: {
 		environment: "jsdom",
-    globals: true
+		globals: true,
 	},
 });
